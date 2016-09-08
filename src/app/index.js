fbb48e8badfa85d2febd9a49e96cf87744f52e58
ext026 <giordano.ricci@consulenti.iblbanca.it>
2016-09-08 10:10:56 +0200

    FastClick for mobile devices

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,8 @@
 'use strict';
 import GameBox from './GameBox';
+import fastClick from 'fastclick';
+
+fastClick.attach(document.body);
 
 let Box= new GameBox(document.getElementById("gameContainer"),4);
 document.querySelector('#gameControls button').addEventListener('click',function(){
